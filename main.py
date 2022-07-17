1184c9e217ded6d3d46b54caa8a124356cf34549
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-07-17 17:44:29 +0800

    добавлен файл
    players.ini

---
+++ main.py
@@ -0,0 +1,14 @@
+#Модуль верхнего уровня для учебного проекта: крестики-нолики
+
+#импорт джополнитльных модулей проекта
+#TODO: import
+
+# супер цикл
+while True:
+    command = input('>').lower()
+    if command in ('quit', 'exit', 'q','e'):
+        break
+    elif command in ('new', 'n'):
+        #начало партии
+        pass
+    #elif ....
