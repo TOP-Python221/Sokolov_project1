abbbeb391158fd029a43023962247002b1f62c6c
Gennadiy <renardes@gmail.com>
2022-07-21 08:57:22 +0500

    мелкие правки

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-'''Модуль верхнего уровня для учебного проекта: крестики-нолики'''
+"""Модуль верхнего уровня для учебного проекта: крестики-нолики"""
 
 # импорт дополнительных модулей проекта
-# TODO: import
-# импорт дополнитльных модулей проекта
 # TODO: import
 
 # супер цикл
 while True:
-    command = input('>').lower()
-    if command in ('quit', 'exit', 'q','e'):
+    command = input('> ').lower()
+    if command in ('quit', 'exit', 'q', 'e'):
         break
     elif command in ('new', 'n'):
         # начало партии
         pass
     # elif ....
-
-
-    # elif
