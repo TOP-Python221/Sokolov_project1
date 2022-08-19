1de417947274c3c8faf98cb9db9b81fd80385b89
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-08-20 00:16:39 +0800

    Добавлена функция print_matrix

--- game.py
+++ game.py
@@ -1,8 +1,42 @@
+from typing import TypeVar, List, Tuple
+from shutil import get_terminal_size as gts
+
 import gameset
 # глобальные переменные модуля game
 
 
 BOARD = [[' '] * gameset.DIM for _ in range(gameset.DIM)]
+
+
+
+
+Matrix = TypeVar('Matrix',
+                 Tuple[List[str], List[str], List[str]],
+                 Tuple[Tuple[str], Tuple[str], Tuple[str]],
+                 List[List[str], List[str], List[str]],
+                 List[Tuple[str], Tuple[str], Tuple[str]],)
+def print_matrix(matrix: Matrix, *matrices, rigth=False, ) -> None:
+    """Выводит в stdout игровое поле с ходами либо с другими символами """
+    matrices = (matrix, ) + matrices
+    width_cell =()
+    for matrix in matrices:
+        for row in matrix:
+            width_cell += (max(len(cell) for cell in row) + 2,)
+    width_full = (wdc * (gameset.DIM + 1) - 1 for wdc in width_cell)
+    pad = 5
+    margin = gts()[0] - sum(width_full) - pad * len(matrices) - 1 \
+                if rigth else \
+                0
+    lines = ()
+    for i  in range(gameset.DIM):
+        rows =()
+        for j in gameset.DIM:
+            row = '|'.join(cell.center(width_cell[j])
+                           for cell in matrices[j][i],)
+        lines += ((' '*pad).join(rows),)
+
+
+
 
 
 
